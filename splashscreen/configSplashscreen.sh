5bc13b4b4fb25ef22564353f7a1b588b87b867f0
tehforsch <Tonipeter92@googlemail.com>
2021-02-07 14:49:02 +0100

    update splashscreen - copy file to /opt/splash

--- splashscreen/configSplashscreen.sh
+++ splashscreen/configSplashscreen.sh
@@ -1,7 +1,8 @@
-sudo apt install fbi
 sudo cp config.txt /boot/config.txt
 sudo cp cmdline.txt /boot/cmdline.txt
 sudo systemctl disable getty@tty1
 sudo cp splashscreen.service /etc/systemd/system/splashscreen.service
 sudo systemctl enable splashscreen
+sudo cp ../pics/splash.png /opt/splash.png
+sudo apt install fbi
 echo "requires reboot!"
